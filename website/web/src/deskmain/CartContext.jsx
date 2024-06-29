f80db1f3d3323bd0bb333341ebc4778915e566a3
SabirinMohamudAdan <sabrinadan327@gmail.com>
2024-06-29 10:29:29 -0700

    added   i Was Updated

--- website/web/src/deskmain/CartContext.jsx
+++ website/web/src/deskmain/CartContext.jsx
@@ -1,9 +1,16 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem('cart');
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (product) => {
     setCart([...cart, product]);
