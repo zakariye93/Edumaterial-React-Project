d7d4cf28b84a321f3c1f2aed59ef9eb0a4b76ba6
zakariye93 <maxamedcalo43@gmail.com>
2024-07-05 23:47:35 +0400

    Update Footer.jsx

--- website/web/src/deskmain/Footer.jsx
+++ website/web/src/deskmain/Footer.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
+import React from 'react';
 
- function Footer() {
+const Footer = () => {
   return (
-    <div>
-       <div className='container grid2'>
-          <div className='box'>
-           
-            <p>Lorem ipsum dolor sit amet, ZAKI scing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div>
+    <footer className="bg-gray-200 h-screen p-4 md:p-6 lg:p-8">
+      <div className="container mx-auto p-4 md:p-6 lg:p-8">
+        <div className="flex flex-wrap justify-center mb-4">
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+            <h4 className="text-lg font-bold mb-2">About Us</h4>
+            <p className="text-gray-600">
+            Welcome to EduMaterial, your go-to source for high-quality educational resources. Our mission is to empower educators, students, and parents with comprehensive, curriculum-aligned materials that foster effective learning and teaching.            </p>
           </div>
-
-          <div className='box'>
-            <h2>About Us</h2>
-            <ul>
-              <li>Careers</li>
-              <li>Our Stores</li>
-              <li>Our Cares</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+            <h4 className="text-lg font-bold mb-2">Quick Links</h4>
+            <ul className="list-none mb-4">
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">Home</a>
+              </li>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">About</a>
+              </li>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">Contact</a>
+              </li>
             </ul>
           </div>
-          <div className='box'>
-            <h2>Customer Care</h2>
-            <ul>
-              <li>Help Center </li>
-              <li>How to Buy </li>
-              <li>Track Your Order </li>
-              <li>Corporate & Bulk Purchasing </li>
-              <li>Returns & Refunds </li>
-            </ul>
-          </div>
-          <div className='box'>
-            <h2>Contact Us</h2>
-            <ul>
-              <li>Deyniile, Mogadisho,Somalia </li>
-              <li>Email: uilib.help@gmail.com</li>
-              <li>Phone: +252617171470</li>
+          <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
+            <h4 className="text-lg font-bold mb-2">Social Media</h4>
+            <ul className="list-none mb-4">
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">
+                  <i className="fab fa-facebook-f mr-2" />
+                  Facebook
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">
+                  <i className="fab fa-twitter mr-2" />
+                  Twitter
+                </a>
+              </li>
+              <li className="mb-2">
+                <a className="text-gray-600 hover:text-gray-900" href="#">
+                  <i className="fab fa-instagram mr-2" />
+                  Instagram
+                </a>
+              </li>
             </ul>
           </div>
         </div>
-    </div>
-  )
-}
-export default Footer
+        <p className="text-gray-600 text-center">
+          Copyright 2024 EDUMATERIAL. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
